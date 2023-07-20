ce1d3748eb95ecf9b7078580378093876c6b121b
Barkın ZORLU <83545665+ZorluBarkin@users.noreply.github.com>
2023-07-20 16:21:18 +0300

    Removed License
    From now on this project is only covered under copyright.

--- CityBox/Assets/Scripts/Managers/ObjectManager.cs
+++ CityBox/Assets/Scripts/Managers/ObjectManager.cs
@@ -1,9 +1,6 @@
 ﻿/*  
- * Copyright 2023 Barkın Zorlu 
- * All rights reserved.
- * 
- * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License. 
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ * Copyright June 2022 Barkın Zorlu 
+ * All rights reserved. 
  */
 
 using System.Collections;
